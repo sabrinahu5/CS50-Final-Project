25fe1e04da2e567f14392f78df23b43cc1e21d86
Sabrina Hu <yourgitemail@example.com>
2022-12-01 22:01:50 -0500

    app.py changes

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime, timezone
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure to use SQLite database
+db = SQL("sqlite:///subscribe.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# home page w/ transaction indices
+"""
+@app.route("/")
+@login_required
+def index():
+"""
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
